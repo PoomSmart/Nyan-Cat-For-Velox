19dfebbaafbbac66ee60ace19973d68b00c033e1
PoomSmart <PoomSmart@Thuchapols-iMac.local>
2013-10-05 18:47:58 +0700

    Headers for Velox 1.0+

--- VeloxFolderViewProtocol.h
+++ VeloxFolderViewProtocol.h
@@ -1,7 +1,7 @@
 @protocol VeloxFolderViewProtocol
-+(int)folderHeight;
--(UIView *)initWithFrame:(CGRect)aFrame;
++ (int)folderHeight;
+- (UIView *)initWithFrame:(CGRect)aFrame;
 @optional
--(void)unregisterFromStuff; //Not necessary, use to unregister from notification centers
--(float)realHeight; //Optional, used to make a folder with a dynamic height. Called right after -initWithFrame
+- (void)unregisterFromStuff; //Not necessary, use to unregister from notification centers
+- (float)realHeight; //Optional, used to make a folder with a dynamic height. Called right after -initWithFrame
 @end
